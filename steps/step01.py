dbdfafc92fc2f3266183fccab3144a313afb0898
yuziroppe <syani4732@gmail.com>
2021-03-18 15:03:15 +0900

    iroiro

--- steps/step01.py
+++ steps/step01.py
@@ -45,42 +45,65 @@ class Variable:
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
-            x, y = f.input, f.output
-            x.grad = f.backward(y.grad)
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            print(gxs)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+                print(x.grad)
 
             if x.creator is not None:
                 funcs.append(x.creator)
 
 
 class Function:
-    def __call__(self, input: Variable) -> Variable:
-        x = input.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creator(self)
-        self.input = input
-        self.output = output
-        return output
+    def __call__(self, *inputs: [Variable]):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
 
-    def forward(self, x: np.ndarray):
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs: [np.ndarray]) -> np.ndarray:
         raise NotImplementedError()
 
-    def backward(self, gy: np.ndarray):
+    def backward(self, gys: [np.ndarray]) -> np.ndarray:
         raise NotImplementedError()
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
 
 
 class Square(Function):
-    def forward(self, x: np.ndarray) -> Variable:
+    def forward(self, x: np.ndarray) -> np.ndarray:
         return x ** 2
 
     def backward(self, gy: np.ndarray) -> np.ndarray:
-        x = self.input.data
+        x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
 
 
 class Exp(Function):
-    def forward(self, x: np.ndarray) -> Variable:
+    def forward(self, x: np.ndarray) -> np.ndarray:
         return np.exp(x)
 
     def backward(self, gy: np.ndarray) -> np.ndarray:
@@ -103,6 +126,10 @@ def numerical_diff(f: Function, x: Variable, eps=1e-4) -> np.ndarray:
     return (y1.data - y0.data) / (2 * eps)
 
 
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
 def square(x):
     return Square()(x)
 
@@ -111,7 +138,9 @@ def exp(x):
     return Exp()(x)
 
 
-x = Variable(np.array(0.5))
-y = square(exp(square(x)))
+x = Variable(np.array(3.0))
+y = add(add(x, x), x)
+print('y', y.data)
+
 y.backward()
-print(x.grad)
+print('x.grad', x.grad)
